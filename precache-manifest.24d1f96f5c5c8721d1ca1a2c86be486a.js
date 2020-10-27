f9cce54ff6afa64c6e65c47bce9ab17b3d3fb5a1
sey cole <seyicole>
2020-10-27 16:08:19 +0100

    Updates

---
+++ precache-manifest.24d1f96f5c5c8721d1ca1a2c86be486a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70ccc178218839bc3c17984b8bce7ac",
+    "url": "/resort/index.html"
+  },
+  {
+    "revision": "1b4f2d5d60e10f084149",
+    "url": "/resort/static/css/main.1f5051cd.chunk.css"
+  },
+  {
+    "revision": "bf60aaf9debc13469b4b",
+    "url": "/resort/static/js/2.1727976b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resort/static/js/2.1727976b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4f2d5d60e10f084149",
+    "url": "/resort/static/js/main.d9bb9442.chunk.js"
+  },
+  {
+    "revision": "d126f1492c1ad25eb3d4",
+    "url": "/resort/static/js/runtime-main.1f9fe477.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/resort/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/resort/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/resort/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/resort/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/resort/static/media/room-2.052c7681.jpeg"
+  }
+]);
